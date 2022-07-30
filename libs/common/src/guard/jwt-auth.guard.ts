34ebcc4d4cfec7c898c91d2fc84a3ea658d528e0
Gwon Min Je <62048410+gwon713@users.noreply.github.com>
2022-07-30 16:43:36 +0900

    feat: :sparkles: add jwt exception

--- libs/common/src/guard/jwt-auth.guard.ts
+++ libs/common/src/guard/jwt-auth.guard.ts
@@ -1,5 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ExecutionContext,
+  HttpException,
+  HttpStatus,
+  Injectable,
+  Logger,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { JsonWebTokenError, TokenExpiredError } from 'jsonwebtoken';
+
+import { CustomStatusCode } from '../constant';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  handleRequest(
+    err: any,
+    user: any,
+    info: any,
+    context: ExecutionContext,
+    status: any,
+  ): any {
+    if (info instanceof TokenExpiredError) {
+      Logger.error('TokenExpiredError');
+      throw new HttpException(
+        CustomStatusCode.TOKEN_EXPIRED,
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
+    if (info instanceof JsonWebTokenError || !user) {
+      Logger.error('JsonWebTokenError');
+      throw new HttpException(
+        CustomStatusCode.INVALID_TOKEN,
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
+    try {
+      return super.handleRequest(err, user, info, context, status);
+    } catch (error) {
+      Logger.error(error);
+      throw new HttpException(
+        CustomStatusCode.INVALID_TOKEN,
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
+  }
+}
